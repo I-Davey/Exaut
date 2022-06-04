8524a7eb47fb0e82bed43dc86388137428feeb48
cackl <idaveynz@gmail.com>
2022-06-04 15:13:19 +1000

    Updated a few small things for functionality

---
+++ PluginManager/Plugins/Delete_file.py
@@ -0,0 +1,34 @@
+from .__important.PluginInterface import PluginInterface
+import os
+import ctypes
+import shutil
+
+class Delete_file(PluginInterface):
+    load = True
+    types = {"file":3}
+
+    type_types = {"source":["drag_drop_file", "Select file"], "__Name":"Delete file"}
+    callname = "delfile"
+    hooks_handler = ["log"]
+
+    def load_self(self, hooks):
+        self.logger = hooks["log"]
+        return True
+
+    def main(self,file) -> bool:
+
+        if os.path.exists(file)==False:
+            self.logger.error("file path does not exist: "+file)
+            return False
+        
+        if os.path.isfile(file)==False:
+            self.logger.error("file path is not a file: "+file)
+            return False
+
+        try:
+            os.remove(file)
+            self.logger.success("file deleted: "+file)
+        except Exception as e:
+            self.logger.error(f"Problem deleting {file}: {e}")
+        
+
--- PluginManager/Plugins/Delete_folder.py
+++ PluginManager/Plugins/Delete_folder.py
@@ -29,6 +29,7 @@ class Delete_folder(PluginInterface):
         
         try:
             shutil.rmtree(folder)
+            self.logger.success("folder deleted: "+folder)
         except Exception as e:
             self.logger.error('Failed to delete %s. Reason: %s' % (folder, e))
             return False
