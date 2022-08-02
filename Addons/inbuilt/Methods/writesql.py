fddef6c0eb8a9762a18c59aea798bb6263c9e5e4
cackl <idaveynz@gmail.com>
2022-08-02 18:56:14 +1000

    added writesql functionality to plugins

---
+++ Addons/inbuilt/Methods/writesql.py
@@ -0,0 +1,76 @@
+"""
+Here is an interface for Plugin classes.
+"""
+from .MethodInterface import MethodInterface
+
+
+class writesql(MethodInterface):
+
+    load = True
+    hooks = ["Sqlite", "log"]
+    types = {"queries":list|object}
+    
+    
+    def load_self(self, hooks):
+        self.logger = hooks["log"]
+        self.session = hooks["Sqlite"].session
+        self.tables = hooks["Sqlite"].tables
+        return True
+
+    def main(self, queries) -> bool:
+        if type(queries) is not list:
+            queries = [queries]
+        with self.session.begin() as sess:
+            try:
+                for query in queries:
+                    sess.execute(query).all()
+                self.session.commit()
+                return True
+
+
+            except Exception as e:
+                if "result object does not return rows." not in str(e):
+
+                    self.logger.error(e)
+                    sess.rollback()
+                    return False      
+
+
+            
+
+
+"""
+from .MethodInterface import MethodInterface
+
+
+class writesql(MethodInterface):
+
+    load = True
+    hooks = ["Sqlite", "log"]
+    types = {"queries":list}
+    
+    
+    def load_self(self, hooks):
+        self.engine_data = hooks["Sqlite"].engine_data
+        self.tables = hooks["Sqlite"].tables
+        self.logger = hooks["log"]
+        return True
+
+    def main(self, queries) -> bool:
+        sessionmaker, create_engine, floc = self.engine_data
+        engine = create_engine(floc)
+        session = sessionmaker(bind=engine)
+        with session.begin() as s:
+            try:
+                for query in queries:
+                    x = s.execute(query).all()
+            except Exception as e:
+                if "result object does not return rows." not in str(e):
+                    self.logger.error(e)
+                    s.rollback()
+                    return False      
+            s.commit()
+            return True
+                
+    """
+            
