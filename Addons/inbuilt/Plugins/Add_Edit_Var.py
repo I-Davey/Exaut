dfd5d413de878a36413f279a25ad85267a3e4c22
cackl <idaveynz@gmail.com>
2022-10-12 15:24:12 +1100

    trest fix add_edit_var

--- Addons/inbuilt/Plugins/Add_Edit_Var.py
+++ Addons/inbuilt/Plugins/Add_Edit_Var.py
@@ -110,7 +110,7 @@ class Add_Edit_Var(PluginInterface):
                 else:
                     cur_loc = '*'     
                     
-
+            print(f"updating {self.variable_name} to {self.value} in {cur_loc}")
             self.writesql(update(variables).where(variables.loc == cur_loc).where(variables.form == formname).where(variables.key == self.variable_name).values(value = self.value))
             self.logger.success(f"Variable {self.variable_name} Updated to {self.value}")
             return True
